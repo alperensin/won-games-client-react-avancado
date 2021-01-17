56cc2ec5b958607b3d7d40355bdadd55239e9fd0
alperensin <andreluizperensin@hotmail.com>
2021-01-17 15:26:24 -0300

    Create a helper to render component in test with theme

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,13 +3,9 @@ import { render, screen } from '@testing-library/react';
 import Logo from '.';
 
 describe('<Logo />', () => {
-  it('Should render the heading', () => {
-    const { container } = render(<Logo />);
-
-    expect(
-      screen.getByRole('heading', { name: /Logo/i })
-    ).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
+  it('✔️ Should render a white label by default', () => {
+    // Renderizar o component ´render´
+    // Selecionar o elemento a ser testado ´screen´ (queries) - getByLabel...
+    // Expect - (Assertion / Asserção) - Comparação - Análise (espero que renderize a logo branca)
   });
 });
