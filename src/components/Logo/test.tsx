5f9d759343ef32ffbaa456f0788bc088c69d21c0
alperensin <andreluizperensin@hotmail.com>
2021-01-17 16:32:55 -0300

    Configuring absolute path (Jest). Creating tests to Logo component and new size property

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import Logo from '.';
 
 describe('<Logo />', () => {
-  it('✔️ Should render a white label by default', () => {
+  it('🧪 Should render a white label by default', () => {
     // Renderizar o component ´render´
+    renderWithTheme(<Logo />);
     // Selecionar o elemento a ser testado ´screen´ (queries) - getByLabel...
     // Expect - (Assertion / Asserção) - Comparação - Análise (espero que renderize a logo branca)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    });
+  });
+
+  it('🧪 Should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    });
+  });
+
+  it('🧪 Should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo size="normal" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    });
+  });
+
+  it('🧪 Should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    });
   });
 });
