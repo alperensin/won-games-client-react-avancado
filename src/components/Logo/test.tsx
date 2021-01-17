5ded90fc370cbb27a9636047faa4685352b41c56
alperensin <andreluizperensin@hotmail.com>
2021-01-17 17:36:05 -0300

    Adding styled-media-query in project. Creating hideOnMobile property on Logo component and .text className on SVG text to change logo on screens sizes mobile and desktop

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
+import 'jest-styled-components';
 
 import Logo from '.';
 
@@ -33,5 +34,16 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     });
+  });
+
+  it('🧪 Should render a bigger logo without text if hidenOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    );
   });
 });
